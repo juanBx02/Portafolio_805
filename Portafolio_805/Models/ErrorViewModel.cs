932111364a049db48a29113324fa0a830ed349b9
juan Guillermo <jgbenitez10@misena.edu.co>
2022-03-17 11:56:22 -0500

    Agregar archivos de proyecto.

---
+++ Portafolio_805/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Portafolio_805.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
